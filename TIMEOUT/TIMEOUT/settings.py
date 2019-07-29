54453fd6e42cf1b713f7e6c15b68fea4eb56ad24
wodlf3393 <wodlf3393@naver,com>
2019-07-29 20:09:10 +0900

    0729 2009

--- TIMEOUT/TIMEOUT/settings.py
+++ TIMEOUT/TIMEOUT/settings.py
@@ -33,11 +33,20 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp.apps.MainappConfig',
+
+    # allauth
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    # provider
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +128,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTHENTICATION_BACKENDS = (
+
+    # Needed to login by username in Django admin, regardless of allauth
+    'django.contrib.auth.backends.ModelBackend',
+
+    # 'allauth' specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
--- TIMEOUT/TIMEOUT/urls.py
+++ TIMEOUT/TIMEOUT/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import mainapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', mainapp.views.home, name='home'),
+    path('accounts/', include('allauth.urls')),
 ]
+
