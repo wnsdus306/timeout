f08f3c8dfb69b7055aa861f544a11b300b8ec67a
wodlf3393 <wodlf3393@naver,com>
2019-07-30 16:56:49 +0900

    0730 1656

--- TIMEOUT/mainapp/models.py
+++ TIMEOUT/mainapp/models.py
@@ -2,18 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class User_history(models.Model):
-    history_title = models.CharField(max_length=100)
-    user_history_money = models.IntegerField()
-
-    def __str__(self):
-        return self.history_title
-
 class User_account(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     user_money = models.IntegerField()
-    history = models.ForeignKey(User_history, on_delete=models.CASCADE, null = True)
     
     def __str__(self):
         return self.user.username
+
+class User_history(models.Model):
+    history_title = models.CharField(max_length=100)
+    user_history_money = models.IntegerField()
+    user_ac = models.ForeignKey(User_account, on_delete=models.CASCADE, null = True)
+
+    def __str__(self):
+        return self.history_title
 
