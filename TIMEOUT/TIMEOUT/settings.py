520b6db25fd168e58b1e59c4302645af0ae246d5
junyeon <wnsdus306@likelion.org>
2019-08-19 15:35:47 +0900

    0819 1535

--- TIMEOUT/TIMEOUT/settings.py
+++ TIMEOUT/TIMEOUT/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1%zs0hbc0ixkh$rq4%qxv1qbmjfv9ivg13i-s&uq6djex+*gd2'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
